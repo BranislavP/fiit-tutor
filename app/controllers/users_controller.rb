01f3418b4e9f9f6d7e3fbd1e62e78405c56bebe3
Branislav Pecher <branop95@gmail.com>
2016-04-07 16:18:41 +0200

    Allow users to delete themselves

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ require 'will_paginate/array'
 class UsersController < ApplicationController
   before_action :logged_in_user , only: [:edit, :update, :index, :destroy]
   before_action :correct_user   , only: [:edit, :update]
-  before_action :admin_user     , only: :destroy
 
 
   def new
@@ -23,7 +22,7 @@ class UsersController < ApplicationController
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
-    redirect_to users_url
+    redirect_to root_url
   end
 
   def create
