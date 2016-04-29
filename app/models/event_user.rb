fd6d70a68bb6bd0765d950e50d97bd8f3532d762
Branislav Pecher <branop95@gmail.com>
2016-04-29 21:25:28 +0200

    Add model validations for Comment, Rating and Event_user

--- app/models/event_user.rb
+++ app/models/event_user.rb
@@ -3,4 +3,6 @@ class EventUser < ActiveRecord::Base
   belongs_to :event
 
   validates_uniqueness_of :user_id, :scope => :event_id
+  validates :user_id, presence: true
+  validates :event_id, presence: true
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,16 @@
 class Rating < ActiveRecord::Base
   belongs_to :user, class_name: 'User'
+
+  validates :score, presence: true
+  validates :content, presence: true
+  validates :user_id, presence: true
+  validates :tutor_id, presence: true
+  validate :rating_score
+
+  private
+  def rating_score
+    if score > 10 || score < 0
+      errors.add(:score, "must be between 0 and 10.")
+    end
+  end
 end
