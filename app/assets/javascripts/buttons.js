f5dcc32630e233436a3b0f9b0505d942ceb89ff2
Branislav Pecher <branop95@gmail.com>
2016-05-31 15:27:04 +0200

    Change how clickable buttons work - tabs using a bootstrap

--- app/assets/javascripts/buttons.js
+++ app/assets/javascripts/buttons.js
@@ -1,6 +1,6 @@
-$(document).on('click', 'button', function(){
-    $("[data^='js_button-']").removeClass('clicked');
-    $(this).addClass('clicked');
+$(document).on('click', 'li', function(){
+    $("[data^='js_button-']").removeClass('active');
+    $(this).addClass('active');
     $("div[data^='div-']").removeClass('show').addClass('hidden');
     $("div[data$='"+$(this).attr('data').split('-')[1]+"']").removeClass('hidden').addClass('show');
 });
