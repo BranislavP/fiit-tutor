6ee707d61306f24869a5557edfc13b33a63f7a98
Branislav Pecher <branop95@gmail.com>
2016-06-06 20:29:28 +0200

    Change ordering when showing subjects to use number of requests

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,6 +1,6 @@
 class SubjectsController < ApplicationController
   def index
     @subject = Subject.find_by_sql("SELECT s.id, acronym, name, COUNT(r.id) FROM subjects s LEFT JOIN requests r ON s.id = r.subject_id GROUP BY s.id
-                                   ORDER BY level, s.id")
+                                   ORDER BY count DESC, level, s.id")
   end
 end
