f2ffc8f87e96f520d48ba1911d20646391aaa645
Branislav Pecher <branop95@gmail.com>
2016-06-10 12:37:17 +0200

    Fix javascript error when there are no clickable buttons present

--- app/assets/javascripts/buttons.js
+++ app/assets/javascripts/buttons.js
@@ -1,6 +1,8 @@
 $(document).on('click', 'li', function(){
-    $("[data^='js_button-']").removeClass('active');
-    $(this).addClass('active');
-    $("div[data^='div-']").removeClass('show').addClass('hidden');
-    $("div[data$='"+$(this).attr('data').split('-')[1]+"']").removeClass('hidden').addClass('show');
+    if( $("[data^='js_button-']").length != 0) {
+        $("[data^='js_button-']").removeClass('active');
+        $(this).addClass('active');
+        $("div[data^='div-']").removeClass('show').addClass('hidden');
+        $("div[data$='" + $(this).attr('data').split('-')[1] + "']").removeClass('hidden').addClass('show');
+    }
 });
